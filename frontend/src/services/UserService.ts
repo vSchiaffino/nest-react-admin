0fa65caec4838f9634ca980476d2be6775993767
Valentin Schiaffino <valenschg@gmail.com>
2024-07-20 01:22:12 -0300

    style: fix linting errors

--- frontend/src/services/UserService.ts
+++ frontend/src/services/UserService.ts
@@ -25,14 +25,8 @@ class UserService {
     id: string,
     updateUserRequest: UpdateUserRequest,
   ): Promise<void> {
-    const {
-      firstName,
-      isActive,
-      lastName,
-      password,
-      role,
-      username,
-    } = updateUserRequest;
+    const { firstName, isActive, lastName, password, role, username } =
+      updateUserRequest;
     await apiService.put(`/api/users/${id}`, {
       firstName: firstName || undefined,
       lastName: lastName || undefined,
