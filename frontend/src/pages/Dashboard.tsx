64cb02273e3ef168a7fe4fa2469bccaa90a5becf
Valentin Schiaffino <valenschg@gmail.com>
2024-07-21 16:24:50 -0300

    feat: add latest courses to dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -4,9 +4,17 @@ import UpdateProfile from '../components/dashboard/UpdateProfile';
 import Layout from '../components/layout';
 import statsService from '../services/StatsService';
 import useAuth from '../hooks/useAuth';
+import useCourses from '../hooks/useCourses';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
   const { authenticatedUser } = useAuth();
+  const { courses } = useCourses('', '', {
+    page: 1,
+    perPage: 3,
+    orderBy: 'dateCreated',
+    orderDirection: 'DESC',
+  });
   const { data, isLoading } = useQuery('stats', statsService.getStats);
 
   return (
@@ -37,8 +45,29 @@ export default function Dashboard() {
             </div>
           </div>
         ) : null}
-
-        <UpdateProfile />
+      </div>
+      <div className="card shadow mt-8 p-12">
+        <h2 className="font-semibold text-4xl mb-10">
+          Check Out Our Latest Courses!
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+          {courses.map((course) => (
+            <Link
+              key={course.id}
+              className="card shadow p-4 no-underline text-black"
+              to={`/courses/${course.id}`}
+            >
+              {/* TODO: image config */}
+              <img
+                src="https://via.placeholder.com/150"
+                alt={`Course ${course.name}`}
+                className="w-full h-32 object-cover mb-4"
+              />
+              <h3 className="text-xl font-semibold mb-2">{course.name}</h3>
+              <p className="text-gray-600">{course.description}</p>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   );
