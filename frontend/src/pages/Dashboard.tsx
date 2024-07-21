54ed3b6ce0a4d197d43dee1e2dd27981f39059bd
Valentin Schiaffino <valenschg@gmail.com>
2024-07-21 16:12:51 -0300

    feat: show total users only for admin role

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,8 +3,10 @@ import { useQuery } from 'react-query';
 import UpdateProfile from '../components/dashboard/UpdateProfile';
 import Layout from '../components/layout';
 import statsService from '../services/StatsService';
+import useAuth from '../hooks/useAuth';
 
 export default function Dashboard() {
+  const { authenticatedUser } = useAuth();
   const { data, isLoading } = useQuery('stats', statsService.getStats);
 
   return (
@@ -12,12 +14,15 @@ export default function Dashboard() {
       <div className="mt-5 flex flex-col gap-5">
         {!isLoading ? (
           <div className="flex flex-col sm:flex-row gap-5">
-            <div className="card shadow text-white bg-[#0074d9] flex-1">
-              <h1 className="font-semibold sm:text-4xl text-center mb-3">
-                {data.numberOfUsers}
-              </h1>
-              <p className="text-center sm:text-lg font-semibold">Users</p>
-            </div>
+            {authenticatedUser.role === 'admin' && (
+              <div className="card shadow text-white bg-[#0074d9] flex-1">
+                <h1 className="font-semibold sm:text-4xl text-center mb-3">
+                  {data.numberOfUsers}
+                </h1>
+                <p className="text-center sm:text-lg font-semibold">Users</p>
+              </div>
+            )}
+
             <div className="card shadow text-white bg-[#4a4aff] flex-1">
               <h1 className="font-semibold sm:text-4xl mb-3 text-center">
                 {data.numberOfCourses}
